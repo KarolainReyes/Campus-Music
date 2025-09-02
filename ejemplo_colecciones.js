a01a0551db65b55cc2a7d4969753d424e3b73d0d
Karolain Reyes <reyeskarol0412@gmail.com>
2025-09-01 20:21:37 -0500

    feat: :tada: Agregue ejemplos para colecciones de datos iniciales: estudiantes, profesores, ubicaciones, cursos, inscripciones, instrumentos y reservaciones.
    Agregue ejemplos de colecciones iniciales
    
    by@KarolainReyes

---
+++ ejemplo_colecciones.js
@@ -0,0 +1,79 @@
+//coleccion estudiantes
+
+[
+{
+  "documento": "12345678",        // unico
+  "nombre_completo": "Andresito Leal",
+  "contacto": 
+        { "email": "andres07@gmail.com" ,
+          "telefono": "+57 3224567890" },
+  "nivel_musical": "principiante",       // [principiante|intermedio|advanzado]
+},
+
+//coleccion profesores
+{
+  
+  "documento": "87654321",        // unico
+  "nombre_completo": "Juan Marinio",
+  "especialidad": ["piano","teoria"],
+  "anios_experiencia": 8,
+
+},
+//coleccion sedes
+{
+  "ciudad": "Bogotá",
+  "direccion": "Cra 7 # 12-34",
+  "capacidad": 300,
+},
+
+//coleccion cursos
+
+{
+
+  "sedeId": ObjectId("..."),
+  "profesorId": ObjectId("..."),
+  "instrumento": "piano",            // indice
+  "nivel": "principiante",
+  "semanas_duracion": 12,
+  "programa": {                     
+    "dia_semana": "Martes",               // lunes, martes..
+    "hora_inicio": "18:00",               
+    "hora_fin": "20:00"
+  },
+  "precio": 600000,                  // COP
+  "capacidad": 12,
+  "cupos_disponibles": 12,             // decrece con inscripciones
+},
+
+//coleccion inscripciones
+
+{
+  "estudianteId": ObjectId("..."),
+  "cursoId": ObjectId("..."),
+  "sedeId": ObjectId("..."),      // denormalizado
+  "profesorId": ObjectId("..."),     // denormalizado
+  "estado": "activo"                // [activo|cancelado|completado]
+},
+
+//coleccion instrumentos
+
+{
+  "sedeId": ObjectId("..."),
+  "tipo_instrumento": "piano",                  // indice compuesto (campusId,type)
+  "total": 8,
+  "disponibles": 8                   
+},
+
+//reservas instrumentos
+
+{
+  "sedeId": ObjectId("..."),
+  "tipo_instrumento": "piano",
+  "estudianteId": ObjectId("..."),
+  "inicio_reserva": ISODate("2025-09-05T15:00:00-05:00"),
+  "fin_reserva": ISODate("2025-09-05T16:00:00-05:00"),
+  "estado": "reservado"              // [reservado|cancelado|finalizado]
+},
+
+
+]
