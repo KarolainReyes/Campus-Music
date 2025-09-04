97432b142d0fd2afb79cd81d98b59afc35534765
Karolain Reyes <reyeskarol0412@gmail.com>
2025-09-03 23:14:54 -0500

    feat: :sparkles: Agregar consultas de agregación para inscripciones y cursos en cada sede

--- aggregation.js
+++ aggregation.js
@@ -3,9 +3,29 @@
 
 //¿Cuántos estudiantes se inscribieron por sede en el último mes?
 
-
+db.inscripciones.aggregate([
+    {$match: {fecha_inscripcion: {$gte: new Date('2025-09-01')}}},
+    {$lookup: {
+        from: 'sedes',
+        localField: 'sedeId',
+        foreignField: '_id',
+        as: 'Sede' 
+    }}, {$group: {_id: '$Sede.nombre_sede', estudiantes_inscritos: {$sum:1}}}
+])
 
 //¿Cuáles son los cursos más demandados en cada sede?
+
+db.inscripciones.aggregate([
+    {$group: {_id: {sedeID: '$sedeId', cursoID: '$cursoId'}, total: {$sum:1}}}, 
+    {$sort: {total:-1}}, {$group:{_id: '$_id.sedeID', curso: {$first: '$_id.cursoID'}, total: {$first: '$total'}}},
+    {$lookup:{
+        from: 'cursos',
+        localField: 'curso',
+        foreignField: '_id',
+        as: 'Curso' 
+    }}
+
+])
 
 //¿Cuál es el ingreso total generado por inscripciones en cada sede?
 
